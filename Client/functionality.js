ead2a51147cffa54daa2b76ea12d650b6f995663
elmax0 <max.biwersi@gmx.de>
2018-02-15 21:02:07 +0100

    added some documentation.

--- Client/functionality.js
+++ Client/functionality.js
@@ -1,24 +1,21 @@
 
 var paint = false;
 
-var xList=new Array();
-var yList=new Array();
-var dList=new Array();
-var strokeList=new Array();
-var thickList = new Array();
+var xList=[];			// List of x- coordinates
+var yList=[];			// List of y- coordinates
+var dList=[];			// List of Dragging- booleans  ---> Ob der Stift erst ansetzt oder eine linie zieht
+var thickList = [];		// List of Thickness
+var strokeList=[];		// List of strokes
 
-var temp=0;
+var temp=0;				// Placeholder variable for splitting the x/y/thick-/stroke- List when using the 'back-button'
 
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
-var r=255;
-var g=255;
-var b=255;
-var a=255;
 
-var mode = 'paint';
+var mode = 'paint';			//standard-mode whend startet. it indicates that you are using the draw-tool
 
+// several buttons
 var clearButton = document.getElementById("button");
 var backButton = document.getElementById("strz");
 var slider = document.getElementById("myRange");
@@ -31,30 +28,24 @@ var thickness = 5;
 canvas.onmousedown = function(e){
 	var posX = e.pageX - this.offsetLeft;
 	var posY = e.pageY - this.offsetTop;
-	if(mode=='paint'){
+	//TODO: More tools like fill, erase etc.
+	if(mode==='paint'){
 		paint = true;
 		addClick(posX,posY);
 		startRecord();
 		redraw();
 	}else{
-		fill(posX,posY);
+		// call fill function
 	}
-}
-function fill(x,y){
+};
 
-    var p = context.getImageData(x, y, 1, 1).data; 
-	var col= (255,255,255);
-	context.putImageData(col,0,0);
-	redraw();
-	
-}
 
 canvas.onmousemove = function(e){
 	if(paint){
 		addClick(e.pageX-this.offsetLeft,e.pageY-this.offsetTop,true);
 		redraw();
 	}
-}
+};
 
 canvas.onmouseleave = function(e){
 	if(paint){
@@ -62,7 +53,7 @@ canvas.onmouseleave = function(e){
 	}
 	paint= false;
 
-}
+};
 
 canvas.onmouseup = function(e){
 	if(paint){
@@ -70,16 +61,21 @@ canvas.onmouseup = function(e){
 	}
 	paint=false;
 
-}
+};
 
 function startRecord(){
 	temp = xList.length;
-	console.log(temp);
+
 }
 
 function stopRecord(){
-	strokeList.push(xList.length - temp);
-	console.log("ho");
+	strokeList.push(xList.length - temp +1);
+	var str=""
+	for(var i=0;i< strokeList.length;i++){
+	str += " " + strokeList[i] + "\n";
+	}
+	console.log(str);
+
 }
 
 function addClick(x,y,dragging){
@@ -92,7 +88,7 @@ function addClick(x,y,dragging){
 function redraw(){
 	context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
   
-	  context.strokeStyle = "#df4b26";
+	  context.strokeStyle = "#b12ddf";
 	  context.lineJoin = "round";
 	  context.lineWidth = 5;
 			
@@ -111,35 +107,36 @@ function redraw(){
 }
 
 clearButton.onclick = function (){
-	xList = new Array();
-	yList = new Array();
-	dList = new Array();
-	strokeList = new Array();
+	xList = [];
+	yList = [];
+	dList = [];
+	strokeList = [];
 	redraw();
-}
+};
 
 
 backButton.onclick = function (){
-	var cut = strokeList[0];
-	
+	console.log("lenght->" + strokeList.length);
+	var cut = strokeList[strokeList.length-1];
+	console.log("back ->" + cut);
 	xList.splice(xList.length-cut,xList.length);
 	yList.splice(yList.length-cut,yList.length);
 	dList.splice(dList.length-cut,dList.length);
 	strokeList.splice(0,1);
 	redraw();
 	
-}
+};
 
 slider.oninput= function () {
 	thickness = this.value;
-}
+};
 
 paintBox.onclick = function (e){
 	mode='paint';
-}
+};
 fillBox.onclick = function (e) {
 	mode = 'fill';
-}
+};
 
 
 
