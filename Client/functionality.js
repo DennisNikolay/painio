1b1ff2385111bb8dcc88c536ce44165fe0799adf
elmax0 <max.biwersi@gmx.de>
2018-02-16 01:53:54 +0100

    added erase-button

--- Client/functionality.js
+++ Client/functionality.js
@@ -1,14 +1,15 @@
-
 var paint = false;
 
+/*
 var xList=[];			// List of x- coordinates
 var yList=[];			// List of y- coordinates
 var dList=[];			// List of Dragging- booleans  ---> Ob der Stift erst ansetzt oder eine linie zieht
 var thickList = [];		// List of Thickness
 var strokeList=[];		// List of strokes
 
-var temp=0;				// Placeholder variable for splitting the x/y/thick-/stroke- List when using the 'back-button'
+var frameCount=0;				// Placeholder variable for splitting the x/y/thick-/stroke- List when using the 'back-button'
 
+*/
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
@@ -18,14 +19,19 @@ var mode = 'paint';			//standard-mode whend startet. it indicates that you are u
 // several buttons
 var clearButton = document.getElementById("button");
 var backButton = document.getElementById("strz");
-var slider = document.getElementById("myRange");
-var paintBox = document.getElementById("paint");
-var fillBox = document.getElementById("fill");
+var eraseButton = document.getElementById("erase");
+var paintButton = document.getElementById("paint");
+var fillButton = document.getElementById("fill");
+var smallButton = document.getElementById('small');
+var normalButton = document.getElementById('normal');
+var bigButton = document.getElementById('big');
 
-var thickness = 5;
+
+var thickness = 3;
+var color='#000000';
 
 var canvasData;
-canvasData = {'xList': [], 'yList': [], 'dList': [], 'thickList': [], 'strokeList': []};
+canvasData = {'xList': [], 'yList': [], 'dList': [], 'thickList': [], 'color':[], 'strokeList': [] , 'frameCount':0};
 
 
 /*
@@ -37,19 +43,31 @@ canvas.onmousedown = function(e){
 	var posX = e.pageX - this.offsetLeft;
 	var posY = e.pageY - this.offsetTop;
 	//TODO: More tools like fill, erase etc.
-	if(mode==='paint'){
-		paint = true;
-		addClick(posX,posY);
-		startRecord();
-		redraw(canvasData);
-	}else{
-		// call fill function
+	paint=true;
+
+	switch(mode){
+		case 'erase':
+		case 'paint':
+            addClick(posX,posY);
+            startRecord();
+            redraw(canvasData);
+            break;
+
+		case 'fill':
+			console.log(fill);
+			break;
+		default:
+
+			//alert('onmousedown-> mode error');
+			alert(mode);
+
 	}
+
 };
 
 
 canvas.onmousemove = function(e){
-	if(paint){
+	if((mode=='paint' || mode=='erase')&&paint){
 		addClick(e.pageX-this.offsetLeft,e.pageY-this.offsetTop,true);
 
 		redraw(canvasData);
@@ -73,17 +91,13 @@ canvas.onmouseup = function(e){
 };
 
 function startRecord(){
-	temp = canvasData.xList.length;
+	canvasData.frameCount = canvasData.xList.length;
 
 }
 
 function stopRecord(){
-	canvasData.strokeList.push(canvasData.xList.length - temp +1);
-	var str=""
-	for(var i=0;i< canvasData.strokeList.length;i++){
-	str += " " + canvasData.strokeList[i] + "\n";
-	}
-	console.log(str);
+	canvasData.strokeList.push(canvasData.xList.length - canvasData.frameCount +1);
+
 
 }
 
@@ -92,6 +106,11 @@ function addClick(x,y,dragging){
 	canvasData.yList.push(y);
 	canvasData.dList.push(dragging);
 	canvasData.thickList.push(thickness);
+	if(mode=='paint'){
+		canvasData.color.push(color);
+	}else{
+		canvasData.color.push('#FFFFFF');
+	}
 }
 
 
@@ -99,13 +118,14 @@ function addClick(x,y,dragging){
 function redraw(data){
 	context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
   
-	  context.strokeStyle = "#b12ddf";
+
 	  context.lineJoin = "round";
 	  context.lineWidth = 5;
 
     for(var i=0; i < data.xList.length; i++) {
         context.lineWidth = data.thickList[i];
         context.beginPath();
+        context.strokeStyle = data.color[i];
         if(data.dList[i] && i){
             context.moveTo(data.xList[i-1], data.yList[i-1]);
         }else{
@@ -140,15 +160,27 @@ backButton.onclick = function (){
 	
 };
 
-slider.oninput= function () {
-	thickness = this.value;
+
+
+smallButton.onclick= function(){
+	thickness = 3;
+	console.log('ha');
+};
+normalButton.onclick= function() {
+	thickness = 7;
+};
+bigButton.onclick = function(){
+	thickness= 15;
 };
 
-paintBox.onclick = function (e){
+paintButton.onclick = function (){
 	mode='paint';
 };
-fillBox.onclick = function (e) {
+fillButton.onclick = function () {
 	mode = 'fill';
+};
+eraseButton.onclick = function(){
+	mode='erase';
 };
 
 
