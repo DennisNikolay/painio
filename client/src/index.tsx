d2aaa64441cf926441b53436f1041afb126300bb
Dennis Nikolay <dennisnikolay@gmx.de>
2018-02-18 02:34:04 +0100

    added console log to client, use JSON.strigify instead writing json string

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,16 +8,15 @@ const lobbyCode = window.location.search.substr(1) || ""; //?aovjoandvoßa
 
 let socket = new WebSocket(server);
 socket.onopen= (e: Event) => {
-    let payload = JSON.parse("{\"requested_lobby\":\"".concat(lobbyCode).concat("\"}"));
+    let payload = JSON.stringify({requested_lobby: lobbyCode});
     socket.send(CLIENT_MESSAGES.CLIENT_LOBBY_STATE_MESSAGES.REQUEST_LOBBY.concat(payload));
     socket.onmessage = (ev: MessageEvent) => {
+        console.log(ev.data);
         if(typeof ev.data == 'string'){
             let cmd = ev.data.substring(0, SERVER_MESSAGE_LENGTH);
             if(cmd == SERVER_MESSAGES.SERVER_LOBBY_STATE_MESSAGES.CONNECTION_ACCEPTED){
                 let payload = JSON.parse(ev.data.substring(SERVER_MESSAGE_LENGTH));
-
                 ReactDOM.render( <DrawingCanvas width={1280} height={1024} socket={socket} user={{identifier: payload.identfier}} />, document.getElementById("example"));
-
             }
         }
     }
